18c06d7313bba7fa559561ece5fcf69165af844c
Segev <segevngr@gmail.com>
2022-02-11 13:33:58 +0200

    w\o accordion

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import './User.css'
 
 const User = ({user, usersList, level}) => {
     const [employees, setEmployees] = useState([]);
-    const [photoLoaded, setPhotoLoaded] = React.useState(false);
+    const [photoLoaded, setPhotoLoaded] = useState(false);
 
     useEffect(() => {
         getEmployees();
@@ -20,11 +20,12 @@ const User = ({user, usersList, level}) => {
         <div>
             <div className="user">
                 <div className="operator">{employees.length > 0 ? '+' : '-'}</div>
-                {user.photo && <img style={photoLoaded ? {} : {display: 'none'}} src={user.photo}
-                         className="photo" onLoad={() => setPhotoLoaded(true)} alt=''/>}
+                {user.photo &&
+                <img style={photoLoaded ? {} : {display: 'none'}} src={user.photo}
+                     className="photo" onLoad={() => setPhotoLoaded(true)} alt=''/>}
                 {(!user.photo || !photoLoaded) &&
-                    <div className="initials">{user.firstName.charAt(0) + user.lastName.charAt(0)}</div>}
-                <div className="name">{user.firstName + ' ' +user.lastName}</div>
+                <div className="initials">{user.firstName.charAt(0) + user.lastName.charAt(0)}</div>}
+                <div className="name">{user.firstName + ' ' + user.lastName}</div>
                 <div className="email">{user.email}</div>
             </div>
             <div style={{marginLeft: `${level}cm`}}>
